f90995c55d52304bc84f862a4e71173a3fbf9d94
Owen Roberts <owen@steets.nl>
2022-10-01 16:59:14 +0200

    Add burger and close to site

---
+++ src/custom/plugins/KiyoTheme/src/Resources/app/storefront/dist/storefront/js/kiyo-theme.js
@@ -0,0 +1,43 @@
+// window.addEventListener('load', (event) => {
+//
+// });
+//
+// const mainNavButton = document.querySelectorAll(".nav-main-toggle-btn");
+// let mainNavClose = document.querySelectorAll(".offcanvas-close");
+//
+// mainNavButton.forEach(button => {
+//     button.addEventListener('click', () => {
+//         console.log('open');
+//
+//         document.querySelectorAll(".main-logo")[0].style.visibility = "hidden"
+//         mainNavClose = document.querySelectorAll(".offcanvas-close");
+//
+//         mainNavClose.forEach(button => {
+//             console.log(button);
+//             button.addEventListener('click', (e) => {
+//                 e.preventDefault();
+//                 console.log('close');
+//                 document.querySelectorAll(".main-logo")[0].style.display = "visible"
+//             });
+//         })
+//     });
+//
+//
+// })
+//
+// // mainNavClose.forEach(button => {
+// //     console.log(button);
+// //     button.addEventListener('click', () => {
+// //         console.log('close');
+// //         document.querySelectorAll(".main-logo")[0].style.display = "visible"
+// //     });
+// // })
+//
+//
+//
+//
+//
+//
+//
+//
+//
